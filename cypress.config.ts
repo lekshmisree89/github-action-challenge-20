83c12338a160aaf245b4f6013654216f8a79f5cb
lekshmisree@gmail.com <lekshmisree89@gmail.com>
2024-11-07 22:02:37 -0500

    cypress config modified

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,5 +11,13 @@ export default defineConfig({
     specPattern: "cypress/component/**/*.cy.{js,ts,jsx,tsx}",
   },
 
+  e2e: {
+    baseUrl: 'http://localhost:3001',
+    setupNodeEvents(on, config) {
+      
+
+      // implement node event listeners here
+    },
+  },
 });
  
