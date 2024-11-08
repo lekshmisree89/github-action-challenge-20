122a89b1863bdebbd96abbff4d5a387edb9bd98d
lekshmisree@gmail.com <lekshmisree89@gmail.com>
2024-11-07 22:00:21 -0500

    cypress config modified

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'cypress';
-import customViteConfig from './vite.config.js';
+import customViteConfig from './vite.config';
 
 export default defineConfig({
   component: {
@@ -11,11 +11,5 @@ export default defineConfig({
     specPattern: "cypress/component/**/*.cy.{js,ts,jsx,tsx}",
   },
 
-  e2e: {
-    baseUrl: 'http://localhost:3001',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
 });
  
